0923a02e05a4cac87245cc8d87eb1a43780937e3
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2023-05-12 11:57:37 +0100

    getting api from Rapidapi

--- src/services/article.js
+++ src/services/article.js
@@ -1,7 +1,25 @@
 import {createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+
+//Code snippets from Rapidapi
+const options = {
+    method: 'GET',
+    url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+    params: {
+      url: 'https://time.com/6266679/musk-ai-open-letter/',
+      length: '3'
+    },
+    headers: {
+      'X-RapidAPI-Key': '94b887ca6cmshd5007fc620e5d92p1c2e73jsnc4acdcdcede8',
+      'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
+    }
+  };
+
 export const articleApi = createApi({
     reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: ''
+    }),
     endpoints: (builder) => ({
         getSummary: builder.query({
             query: (params) => `test`
